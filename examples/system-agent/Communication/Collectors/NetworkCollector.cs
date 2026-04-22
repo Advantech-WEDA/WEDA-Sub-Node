cbbcb969d839f0ed14a4aae81133b1e7cbba6b9b
tony.hsieh <tony.hsieh@advantech.com>
2026-04-22 11:32:20 +0800

    調整log level

--- examples/system-agent/Communication/Collectors/NetworkCollector.cs
+++ examples/system-agent/Communication/Collectors/NetworkCollector.cs
@@ -39,7 +39,7 @@ public class NetworkCollector
                 }
                 catch (PlatformNotSupportedException)
                 {
-                    _logger.LogWarning("NonUnicastPackets not supported for {Name}, using 0", iface.Name);
+                    _logger.LogDebug("NonUnicastPackets not supported for {Name}, using 0", iface.Name);
                 }
 
                 var networkMetric = new NetworkMetrics
